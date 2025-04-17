395286909b9812cb8711f8b1c2999a3436e2994b
doruk-verisoft1 <doruk.verisoft@gmail.com>
2025-04-17 15:19:14 +0300

    first commit

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -15,19 +15,52 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park"
+                    Description = "The one with that big park",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto(){
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited park" },                            
+                        new PointOfInterestDto(){
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Located midtown Manhattan" },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwrep",
-                    Description = "The one with that big cathedral"
+                    Description = "The one with that big cathedral",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto(){
+                            Id = 3,
+                            Name = "Cathadrel of Our Lady",
+                            Description = "Gothic style Cathadrel" },
+                        new PointOfInterestDto(){
+                            Id = 4,
+                            Name = "Antwrep Central Station",
+                            Description = "Center station" },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Paris",
-                    Description = "The one with that big tower"
+                    Description = "The one with that big tower",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto(){
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "Tower in paris" },
+                        new PointOfInterestDto(){
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "Largest museum" },
+                    }
                 }
             };
         }
