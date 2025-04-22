c3b12725bd812dccc3d73e79a5ad8193354bedcd
doruk-verisoft1 <doruk.verisoft@gmail.com>
2025-04-22 14:26:34 +0300

    iş yeri

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -22,7 +22,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(
             int cityıd, int pointOfInterestId)
         {
@@ -41,5 +41,40 @@ namespace CityInfo.API.Controllers
             }
             return Ok(pointOfInterest);
         }
+
+        [HttpPost]
+
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest, PointOfInterestForCreationDto finalPointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                             c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestForCreationDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = finalPointOfInterest.Id
+                },
+                finalPointOfInterest);
+        }
+
     }
 }
--- CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -2,5 +2,8 @@
 {
     public class PointOfInterestForCreationDto
     {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int Id { get; internal set; }
     }
 }
